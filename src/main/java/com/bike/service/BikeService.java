86b529e00f2b86e5429501aff4e1b52a846cbca7
Nestor Milyaev <nestor.milyaev@capita.co.uk>
2021-03-23 15:01:21 +0000

    Bike Service and repository: unit test, no delete implemented

--- src/main/java/com/bike/service/BikeService.java
+++ src/main/java/com/bike/service/BikeService.java
@@ -6,38 +6,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class BikeService {
 
     private final BikeRepository bikeRepository;
-    private List<Bike> repository;
 
     @Autowired
     public BikeService(BikeRepository bikeRepository) {
         this.bikeRepository = bikeRepository;
     }
 
-    public List<Bike> getList(){
-        Bike bike1 = new Bike(UUID.randomUUID(), "Raleigh", "Pioneer", BigDecimal.valueOf(80.00));
-        Bike bike2 = new Bike(UUID.randomUUID(), "Dawes", "Galaxy", BigDecimal.valueOf(100.00));
-        repository = Arrays.asList(bike1, bike2);
-        log.info("Bikes:{}", repository.stream().map(Objects::toString).collect(Collectors.joining("\n")));
-        return repository;
+    public List<Bike> getList() {
+        return bikeRepository.findAll();
     }
 
     public Bike getById(UUID id) {
-        return repository.stream().filter(b->b.getId().equals(id)).findFirst().orElse(null);
+        return bikeRepository.getOne(id);
     }
 
-    public Bike addNewBike(Bike bike){
+    public Bike addNewBike(Bike bike) {
         return bikeRepository.save(bike);
+    }
+
+    /**
+     * Deletes a bike by id
+     * @param bikeId - searched id
+     * @return true or false depending on the success of the operation (i.e. whether the entry is found)
+     */
+    public boolean deleteBike(UUID bikeId) {
+        Bike bike = bikeRepository.getOne(bikeId);
+        if (bike ==null){
+            return false;
+        }
+        bikeRepository.deleteById(bikeId);
+        return true;
     }
 }
--- src/test/java/com/bike/service/BikeServiceTest.java
+++ src/test/java/com/bike/service/BikeServiceTest.java
@@ -2,17 +2,22 @@ package com.bike.service;
 
 import com.bike.model.Bike;
 import com.bike.repository.BikeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class BikeServiceTest {
@@ -23,9 +28,21 @@ public class BikeServiceTest {
     @InjectMocks
     BikeService service;
 
+    private List<Bike> bikes;
+
+    @BeforeEach
+    private void setUpRepository() {
+        bikes = new ArrayList<>();
+        Bike bike1 = new Bike(UUID.randomUUID(), "Raleigh", "Pioneer", BigDecimal.valueOf(80.00));
+        Bike bike2 = new Bike(UUID.randomUUID(), "Dawes", "Galaxy", BigDecimal.valueOf(100.00));
+        bikes.add(bike1);
+        bikes.add(bike2);
+    }
+
     @Test
     public void shouldGetAllBikes() {
         // given
+        given(bikeRepository.findAll()).willReturn(bikes);
 
         // when
         List<Bike> bikes = service.getList();
@@ -36,6 +53,44 @@ public class BikeServiceTest {
         assertBikesTheSame(bike1, bikes.get(0));
         assertBikesTheSame(bike2, bikes.get(1));
     }
+
+    @Test
+    public void shouldGetBikeById() {
+        // given
+        UUID bikeId = bikes.get(0).getId();
+        given(bikeRepository.getOne(bikeId)).willReturn(bikes.get(0));
+
+        // when
+        Bike bike = service.getById(bikeId);
+
+        //then
+        Bike bike1 = new Bike(bikeId, "Raleigh", "Pioneer", BigDecimal.valueOf(80.00));
+        assertBikesTheSame(bike1, bike);
+    }
+
+    @Test
+    public void shouldCreateNewBike() {
+        // given
+        UUID bikeId = UUID.randomUUID();
+        Bike bike3 = new Bike(bikeId, "Claud Butler", "Echelon", BigDecimal.valueOf(180.00));
+        given(bikeRepository.save(bike3)).will(new Answer() {
+            public Object answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                Bike bike = (Bike) args[0];
+                bikes.add(bike);
+                return bike;
+            }
+        });
+
+        // when
+        Bike bike = service.addNewBike(bike3);
+
+        //then
+        assertEquals(3, bikes.size());
+        assertBikesTheSame(bike3, bikes.get(2));
+        assertBikesTheSame(bike, bikes.get(2));
+    }
+
 
     private void assertBikesTheSame(Bike bike1, Bike bike2) {
         assertEquals(bike1.getMake(), bike2.getMake());
