77ee30fa63289e90b15bf1294a3d5ead078b68aa
nmilyaev <n.milyaev@yahoo.co.uk>
2021-06-16 16:27:07 +0100

    TidyUp FlyWay scripts: Change sequence and remove the redundant alter script

--- src/main/resources/db/migration/V2__create-table-bike.sql
+++ src/main/resources/db/migration/V2__create-table-bike.sql
@@ -6,5 +6,8 @@ create table if not exists bike (
    deposit numeric(8, 2),
    daily_rate numeric(8, 2),
    owner_id uuid not null,
+   constraint bike_owner_id
+       foreign key (owner_id)
+       references mybike_user,
    primary key (bike_id)
 )
--- src/main/resources/db/migration/V3__update-table-bike-add-foreign-key-to-user.sql
+++
@@ -1,4 +0,0 @@
-alter table bike
-   add constraint bike_owner_id
-       foreign key (owner_id)
-       references mybike_user;
